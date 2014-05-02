f615962f649f4479e4d878517fc401894b263a7c
Jerry Dunmire <jdunmire@pioneer-pad.com>
2014-05-02 16:03:26 -0700

    Added configuration checks
    modified:   pkg_server_files.sh

--- pkg_server_files.sh
+++ pkg_server_files.sh
@@ -2,11 +2,24 @@
 #
 # pkg_server_files.sh - create tarball of files to be transferred to server
 #
-set -e
 
 EASYRSA_KEYDIR="./EasyRSA-2.2.2/keys/"
 KEY_FILES="ca.crt dh2048.pem myserver.crt myserver.key"
 CONFIG_FILES="server.conf dynamicSetup.sh setup_server.sh"
+
+# check to make sure the files have been configured
+grep -v '#' dynamicSetup.sh | grep -q 'DDNS_URL='
+if [ $? -ne 0 ]
+then
+    echo "WARNING: DDNS_URL has not been defined in dynamicSetup.sh."
+    echo "         No dynamic DNS updates will be possible."
+fi
+
+if  grep -q SERVER_DNS_OR_IP client.ovpn 
+then
+    echo "WARNING: Configuration not complete. Server not specified in"
+    echo "         client.ovpn."
+fi
 
 PKG_DIR=${PWD}/server_files
 mkdir ${PKG_DIR}
